ff2205b0aa6e71ea36f37b9da8796954d65d5a3a
seyeong.kim <kimseyeong97@gmail.com>
2022-01-12 23:55:33 +0900

    study using static file

--- express/app.js
+++ express/app.js
@@ -7,6 +7,10 @@ app.get('/', function(req, res) {
 app.get('/login', function(req, res) {
     res.send('Login please');
 });
+app.get('/route', function(req, res) {
+    res.send('Hello Router, <img src="/route.png">')
+})
+app.use(express.static('public'));
 
 app.listen(3000, function() {
     console.log('Connected 3000 port!');    
