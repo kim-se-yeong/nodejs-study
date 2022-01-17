efb1b413f1f132578c45bad65a64f7bc68a1c98d
seyeong.kim <kimseyeong97@gmail.com>
2022-01-17 22:35:55 +0900

    studying static, dynamic web

--- express/app.js
+++ express/app.js
@@ -10,6 +10,34 @@ app.get('/login', function(req, res) {
 app.get('/route', function(req, res) {
     res.send('Hello Router, <img src="/route.png">')
 })
+
+//동적
+app.get('/dynamic', function(req, res) {
+    var lis = '';
+    for (var i = 0; i < 5; i++) {
+        lis = lis + '<li>coding</li>';
+    }
+
+    var time = Date();
+    
+    var output = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title></title>
+    </head>
+    <body>
+        Hello, Dynamic!
+        <ul>
+            ${lis}
+        </ul>
+        ${time}
+    </body>
+    </html>`;
+    res.send(output);
+})
 app.use(express.static('public'));
 
 app.listen(3000, function() {
