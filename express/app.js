8a37c00af881c620c1dc46ce9802701c7d532ce6
seyeong.kim <kimseyeong97@gmail.com>
2022-01-28 22:13:22 +0900

    study query string

--- express/app.js
+++ express/app.js
@@ -50,6 +50,22 @@ app.get('/dynamic', function(req, res) {
 })
 app.use(express.static('public'));
 
+//use query string
+app.get('/topic', function(req, res) {
+    var topics = [
+        'Javascript is...',
+        'Nodejs is...',
+        'Express is...'
+    ];
+    var output = `
+        <a href="/topic?id=0">Javascript</a><br>
+        <a href="/topic?id=1">Nodejs</a><br>
+        <a href="/topic?id=2">Express</a><br><br>
+        ${topics[req.query.id]}
+    `
+    res.send(output);
+});
+
 app.listen(3000, function() {
     console.log('Connected 3000 port!');    
 });
