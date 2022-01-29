f9ab7f9a121e5405e2afbb40206cc9f23953451a
seyeong.kim <kimseyeong97@gmail.com>
2022-01-30 00:32:38 +0900

    studying create web app2

--- web/app_file.js
+++ web/app_file.js
@@ -11,6 +11,33 @@ app.get('/topic/new', (req, res) => {
     res.render('new');
 });
 
+app.get('/topic', (req, res) => {
+    fs.readdir('data', (err, files) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        res.render('view', {topics:files});
+    });
+});
+
+app.get('/topic/:id', (req, res) => {
+    var id = req.params.id;
+    fs.readdir('data', (err, files) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        fs.readFile('data/' + id, 'utf8', (err, data) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Internal Server Error');
+            }
+            res.render('view', {title:id, topics:files, description:data});
+        });
+    });
+});
+
 app.post('/topic', (req, res) => {
     var title = req.body.title;
     var description = req.body.description;
