eb2569a9095d182d10fd4a9bf24a9ca1e19a4d95
seyeong.kim <kimseyeong97@gmail.com>
2022-02-07 23:17:56 +0900

    study about create page using express

--- express/main.js
+++ express/main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const qs = require('querystring');
 const template = require('./lib/template.js');
 
 app.get('/', (req, res) => {
@@ -18,6 +19,41 @@ app.get('/', (req, res) => {
 
 app.get('/page/:pageId', (req, res) => {
     res.send(req.params.pageId);
+});
+
+app.get('/create', (req, res) => {
+    fs.readdir('data', (err, fileList) => {
+        var title = 'WEB - create';
+        var list = template.list(fileList);
+        var html = template.HTML(title, list, `
+            <form action="/create" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p>
+                    <textarea name="description" placeholder="description"></textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+            </form>
+        `, '');
+        res.send(html);
+    });
+});
+
+app.post('/create', (req, res) => {
+    var body = '';
+    req.on('data', (data) => {
+        body = body + data;
+    });
+    req.on('end', () => {
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        fs.writeFile(`data/${title}`, description, 'utf8', (err) => {
+            res.writeHead(302, {Location: `/?id=${title}`});
+            res.end();
+        });
+    });
 });
 
 app.listen(3000, () => {
