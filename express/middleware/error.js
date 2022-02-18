1f1ec294149fba45fe2d278a164036ef460e5685
kim-se-yeong <kimseyeong97@gmail.com>
2022-02-18 17:21:44 +0900

    study middleware of error level

---
+++ express/middleware/error.js
@@ -0,0 +1,24 @@
+import express from "express";
+const app = express();
+
+app.get('/', (req, res, next) => {
+    next(error);    
+});
+
+app.get('/user/:id', (req, res, next) => {
+    if (isNaN(req.params.id)) {
+        res.statusCode = 404;
+        next(error);
+    } else {
+        res.send(`my number is ${req.params.id}`);
+    }
+});
+
+app.use((err, req, res, next) => {
+    if (res.statusCode == 404)
+        res.status(404).send('page not found!');
+    else
+        res.status(500).send('somthing broken!');
+});
+
+app.listen('3001', () => console.log('middleware server running!'));
