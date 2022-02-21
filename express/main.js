85c5cd7c9a32838fb45ea416d5d9b57a88eaf9db
kim-se-yeong <kimseyeong97@gmail.com>
2022-02-21 18:05:35 +0900

    use router

--- express/main.js
+++ express/main.js
@@ -2,9 +2,9 @@
 import express from './lib/express.js';
 const app = express();
 import fs from 'fs';
-import {HTML, list as _list} from './lib/template.js';
 import compression from 'compression';
 import topicRouter from './routes/topic.js';
+import indexRouter from './routes/index.js';
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -20,19 +20,8 @@ app.get('*', (req, res, next) => {
     });
 });
 
+app.use('/', indexRouter);
 app.use('/topic', topicRouter);
-
-app.get('/', (req, res) => {
-    var title = 'Welcome';
-    var description = 'Hello, Node.js'
-    var list = _list(req.list);
-    var html = HTML(title, list,
-        `<h2>${title}</h2>${description}
-        <img src="/hello.png" style="width:100px; display:block">`,
-        `<a href="/create">create</a>`,
-    );
-    res.send(html);
-});
 
 app.use((req, res, next) => {
     res.status(404).send('Sorry cant find that!');
---
+++ express/routes/index.js
@@ -0,0 +1,18 @@
+import express from '../lib/express.js';
+const router = express.Router();
+
+import {HTML, list as _list} from '../lib/template.js';
+
+router.get('/', (req, res) => {
+    var title = 'Welcome';
+    var description = 'Hello, Node.js'
+    var list = _list(req.list);
+    var html = HTML(title, list,
+        `<h2>${title}</h2>${description}
+        <img src="/hello.png" style="width:100px; display:block">`,
+        `<a href="/create">create</a>`,
+    );
+    res.send(html);
+});
+
+export default router;
